14600e9a771f734905ae724ddd72092b2756da09
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-06 16:16:59 +0300

    style: start styling the CV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,31 @@
 const EMAIL = "seesmwork@gmail.com";
 const LIST_PROPERTIES = "list-disc list-inside";
+const HEADING_TWO_CLASSES = "text-2xl";
+const HEADING_THREE_CLASSES = "text-lg";
 
 export default function Page() {
   return (
-    <div className="p-3 flex flex-col gap-3">
+    <div className="p-3 flex flex-col gap-5">
       {/* A title. */}
-      <article>
-        <h1>Oleh Onyshchenko</h1>
+      <article className="mb-3">
+        <h1 className="text-3xl">Oleh Onyshchenko</h1>
         <p>Christian Developer & Translator</p>
       </article>
 
       {/* Interests */}
       <article>
-        <h2>Interests</h2>
+        <h2 className={HEADING_TWO_CLASSES}>Interests</h2>
 
-        <h3>Professional</h3>
-        <p>My main technical interests are:</p>
+        <h3 className={HEADING_THREE_CLASSES}>Professional</h3>
+        <p className="mt-2">My main technical interests are:</p>
         <ul className={LIST_PROPERTIES}>
           <li>embedded systems</li>
           <li>city planning</li>
           <li>frontend development</li>
         </ul>
 
-        <h3>Personal</h3>
-        <p>And my main personal interests are:</p>
+        <h3 className={HEADING_THREE_CLASSES + " mt-3"}>Personal</h3>
+        <p className="mt-2">And my main personal interests are:</p>
         <ul className={LIST_PROPERTIES}>
           <li>Christian faith</li>
           <li>church going</li>
@@ -33,28 +35,28 @@ export default function Page() {
 
       {/* Projects */}
       <article>
-        <h2>Projects</h2>
+        <h2 className={HEADING_TWO_CLASSES}>Projects</h2>
         <p>Here are my personal projects.</p>
 
-        <h3>Class Opening Automator</h3>
+        <h3 className={HEADING_THREE_CLASSES}>Class Opening Automator</h3>
         <em>Python</em>
         <p>This is a simple app that opens classes in their scheduled time.</p>
 
-        <h3>YouVersion Volunteering</h3>
+        <h3 className={HEADING_THREE_CLASSES}>YouVersion Volunteering</h3>
         <em>English to Ukrainian translations</em>
         <p>
           Had some experience volunteering at YouVersion, translating their
           app’s content to Ukrainian
         </p>
 
-        <h3>Bible Automations</h3>
+        <h3 className={HEADING_THREE_CLASSES}>Bible Automations</h3>
         <em>Python</em>
         <p>
           This was a script where I automated Bible text formatting and copying
           to different places.
         </p>
 
-        <h3>Online Trucks Store</h3>
+        <h3 className={HEADING_THREE_CLASSES}>Online Trucks Store</h3>
         <em>Python, NiceGUI</em>
         <p>
           This was a diploma’s work where I used NiceGUI library to create a
@@ -64,7 +66,7 @@ export default function Page() {
 
       {/* Contacts */}
       <article>
-        <h2>Contacts</h2>
+        <h2 className={HEADING_TWO_CLASSES}>Contacts</h2>
         <p>To get in touch use these:</p>
         <ul className={LIST_PROPERTIES}>
           <li>
